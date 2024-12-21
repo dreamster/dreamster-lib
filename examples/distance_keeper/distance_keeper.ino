083a017ecc8e61b81933d070236a7f4056a0a1ff
Misael.K <MisaelK@users.noreply.github.com>
2024-12-20 22:04:53 -0300

    Improved distance_keeper
    Updated example for newer lib.
    Robot auto-calibrates safe values when starting, and sets a small tolerance for the safe vs unsafe values.

--- examples/distance_keeper/distance_keeper.ino
+++ examples/distance_keeper/distance_keeper.ino
@@ -12,14 +12,20 @@ int dist_us_a;
 uint16_t ir_left;
 uint16_t ir_right;
 
-const int floor_limit = 15;
-
-// use this to calibrate the motors
-const int left_motor_offset = -2;
-const int right_motor_offset = 1;
+int safe_floor_value;
 
 void setup() {
   robot.setup();
+  robot.calibrate_motors_zero(-5, -5);
+
+  delay(100);
+
+  // set floor limit as initial position
+  robot.read_ir(ir_left, ir_right);
+  // if one of the sensors is dead, copy the value from the working one
+  if (ir_right == 0) ir_right = ir_left;
+  if (ir_left == 0) ir_left = ir_right;
+  safe_floor_value = (ir_left + ir_right) / 2;
 }
 
 void loop() {
@@ -32,19 +38,25 @@ void loop() {
   // if one of the sensors is dead, copy the value from the working one
   if (ir_right == 0) ir_right = ir_left;
   if (ir_left == 0) ir_left = ir_right;
+  int avg_ir = (ir_left + ir_right) / 2;
+  int abs_diff = abs(avg_ir - safe_floor_value);
 
   // print sensors for debugging
   Serial.print(dist_us_a); Serial.print(" ");
   Serial.print(ir_left);   Serial.print(" ");
   Serial.print(ir_left);  Serial.print(" ");
+  Serial.print(safe_floor_value);  Serial.print(" ");
+  Serial.print(abs_diff);  Serial.print(" ");
   Serial.println("");
 
+  dist_us_a *= 10;
+
   // If the infrared sensors show the table continue, check for danger
-  if (ir_left > floor_limit && ir_right > floor_limit) {
+  if (abs_diff < 50) {
     // If too close, go backwards
     if (dist_us_a < 80) {
       left_motor_speed = -20;
-      right_motor_speed = -20;
+      right_motor_speed = -20; 
     }
     // If we are far enough then stop
     else if (dist_us_a > 120 && dist_us_a < 200) {
@@ -64,8 +76,6 @@ void loop() {
       right_motor_speed = 0;
     }
   }
-  robot.move(left_motor_speed + left_motor_offset, right_motor_speed + right_motor_offset);
-  robot.update();
-  
-  //delay(100); // to ease debugging
+  robot.move(left_motor_speed, right_motor_speed);
+  delay(1); // ensures cycles every 1 ms at least
 }
